23be3413dcbb7a3b2c8becbf7502961ef0c9bc42
Gordon H Graham <theboss@gordonhgraham.com>
2016-12-27 19:26:53 -0600

    add components for header and welcome bar

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// import library to help create Component
+import React from 'react'
+import { AppRegistry, View } from 'react-native'
+import Header from './src/components/Header'
+import WelcomeBar from './src/components/WelcomeBar'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const DestinationFit = () => (
+  //add style={{ flex: 1 }} to root view below
+  <View>
+    <Header headerText={'DestinationFit'}/>
+    <WelcomeBar />
+  </View>
+)
 
-export default class DestinationFit extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('DestinationFit', () => DestinationFit);
+AppRegistry.registerComponent('DestinationFit', () => DestinationFit)
---
+++ src/components/WelcomeBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+// import Section from './Section'
+import Button from './Button'
+
+const WelcomeBar = (props) => {
+  const { textStyle, containerStyle } = styles
+
+  return (
+  <View style={containerStyle}>
+    <View>
+      <Text>
+        Welcome, James!
+      </Text>
+    </View>
+    <View>
+      <Button onPress={() => console.log('Button Pressed!')}>
+        Log Out
+      </Button>
+    </View>
+  </View>
+  )
+}
+
+const styles = {
+  textStyle: {
+    fontSize: 20,
+  },
+  containerStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+}
+
+export default WelcomeBar
